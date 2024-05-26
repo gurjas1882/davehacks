0fdd6b1f0d238942d013f5189b8109e7e6b9f95c
gurjas1882 <gurjas1882@gmail.com>
2024-05-26 17:20:39 -0400

    all

--- src/pages/QuestionChoicePage.js
+++ src/pages/QuestionChoicePage.js
@@ -8,7 +8,7 @@ function QuestionChoicePage() {
 
   return (
     <div className='optionsContainer'>
-      <h1 className="logo">studybuddy</h1>
+      <h1 className="logo" onClick={() => navigate('/')}>studybuddy</h1>
       <h1 className='optionsTitle'>what would you like to do today?</h1>
       <div>
       <button className="optionQuestion" onClick={() => navigate('/ask-question')}>Ask a Question</button>
