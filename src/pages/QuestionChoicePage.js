94055fd55108325ad45a2eece2eaf14eb3988f31
gurjas1882 <gurjas1882@gmail.com>
2024-05-26 15:43:36 -0400

    dw

--- src/pages/QuestionChoicePage.js
+++ src/pages/QuestionChoicePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import "./QuestionChoicePage.css";
+
 
 function QuestionChoicePage() {
   const navigate = useNavigate();
 
   return (
     <div>
+      <h1 className="logo">studybuddy</h1>
       <h1>Choose Action</h1>
       <button onClick={() => navigate('/ask-question')}>Ask a Question</button>
       <button onClick={() => navigate('/test')}>Test Your Knowledge</button>
